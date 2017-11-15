aec5a8e44025a7ab7f673602295bdef66171fcfc
Adam Smith <hagansmith@gmail.com>
2017-11-15 09:12:49 -0600

    add google auth, login button, UID set in $rootScope

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,8 +1,8 @@
 "use strict";
 
-// app.run(function(FIREBASE_CONFIG){
-//   firebase.initializeApp(FIREBASE_CONFIG);
-// });
+app.run(function(FIREBASE_CONFIG){
+  firebase.initializeApp(FIREBASE_CONFIG);
+});
 
 app.config(function($routeProvider){
   $routeProvider
